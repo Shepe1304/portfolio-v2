fcb812e65578de229df4abe0a03e15a8fa54e6a9
Quynh-Giang Ho <hqgiang1304@gmail.com>
2024-04-06 17:42:56 +0700

    updated Resume.js

--- src/components/pages/resume/Resume.js
+++ src/components/pages/resume/Resume.js
@@ -7,13 +7,18 @@ import nightsky from "../../assets/images/nightsky.png";
 const Resume = () => {
   return (
     <div className="resume">
-      <Header color="white" backgroundColor="#2e3047"/>
+      <Header color="white" backgroundColor="#2e3047" />
       <div className="resume--main">
-        <img src={nightsky} alt="" className="homepage--background" />
-        <div className="resume--heading">SHEPE'S RESUME</div>
-        <div className="resume--download_button"></div>
+        <div className="resume--preview_container">
+          <iframe
+            className="resume--preview"
+            src="https://firebasestorage.googleapis.com/v0/b/portfolio-cdf0d.appspot.com/o/files%2FQUYNH-GIANG-HO_RESUME.pdf?alt=media&token=a9b9759a-66c2-4d1e-a6fc-c9c51a180634"
+            frameborder="0"
+            loading="lazy"
+          ></iframe>
+        </div>
       </div>
-      <Footer color="white" backgroundColor="#2e3047"/>
+      <Footer color="white" backgroundColor="#2e3047" />
     </div>
   );
 };
